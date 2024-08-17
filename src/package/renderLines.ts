100f81b309e92bb4aad230e1394186894202430a
Jayden Sim <39719057+jaydensim@users.noreply.github.com>
2024-08-17 18:23:50 +1000

    updated lines

--- src/package/renderLines.ts
+++ src/package/renderLines.ts
@@ -37,7 +37,7 @@ export class lineRenderer {
       const shouldReverseColor =
         distance([0, 0], segment.geometry[0]) >
         distance([0, 0], segment.geometry[segment.geometry.length - 1])
-      const colors = !shouldReverseColor
+      const colors = shouldReverseColor
         ? [...segment.colors.sort()].reverse()
         : segment.colors.sort()
 
